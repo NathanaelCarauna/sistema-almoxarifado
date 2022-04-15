abfde88fcd466d470beb2adf7db476261303edcd
Edgarvital <edgar.tecnico@hotmail.com>
2022-04-15 19:44:22 -0300

    Alteração na unidade

--- resources/views/solicitacao/solicita_material.blade.php
+++ resources/views/solicitacao/solicita_material.blade.php
@@ -1,4 +1,3 @@
-
 @extends('templates.principal')
 
 @section('title') Solicitar Material @endsection
@@ -19,7 +18,7 @@
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <ul>
                 @foreach($errors->all() as $erro)
-                        <li>{{ $erro }}</li>
+                    <li>{{ $erro }}</li>
                 @endforeach
             </ul>
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -65,11 +64,11 @@
         @csrf
         <table id="tableMaterial" class="table table-hover table-responsive-md" style="margin-top: 10px">
             <thead style="background-color: #151631; color: white; border-radius: 15px">
-                <tr>
-                    <th scope="col">Material</th>
-                    <th scope="col" style="text-align: center">Quantidade</th>
-                    <th scope="col" style="text-align: center">Ações</th>
-                </tr>
+            <tr>
+                <th scope="col">Material</th>
+                <th scope="col" style="text-align: center">Quantidade</th>
+                <th scope="col" style="text-align: center">Ações</th>
+            </tr>
             </thead>
             <tbody></tbody>
         </table>
@@ -83,7 +82,8 @@
                 <div class="form-group">
                     <label for="inputNomeReceptor">Nome</label>
                     <input type="hidden" id="nomeReceptor" name="nomeReceptor" value="{{Auth::user()->nome}}">
-                    <input type="text" class="form-control" id="inputNomeReceptor" onkeypress="return onlyLetters(event,this);" maxlength="100" name="nomeReceptor" value="{{Auth::user()->nome}}" disabled="true">
+                    <input type="text" class="form-control" id="inputNomeReceptor" onkeypress="return onlyLetters(event,this);" maxlength="100" name="nomeReceptor" value="{{Auth::user()->nome}}"
+                           disabled="true">
                 </div>
             </div>
             <div class="form-group col-md-3">
@@ -108,42 +108,42 @@
         <input type="hidden" id="dataTableMaterial" name="dataTableMaterial" value="">
         <input type="hidden" id="dataTableQuantidade" name="dataTableQuantidade" value="">
 
-        <Button class="btn btn-secondary" type="button" onclick="location.href = '../' "> Cancelar </Button>
+        <Button class="btn btn-secondary" type="button" onclick="location.href = '../' "> Cancelar</Button>
         <button id="solicita" class="btn btn-success" disabled onclick="return setValuesRowInput()">Solicitar</button>
     </form>
 
     <div class="modal fade" id="detalhesSolicitacao" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
         <div class="modal-dialog modal-xl" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="modalLabel" style="color:#151631">Editar Solicitação</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body">
-                <div id="modalBody">
-                    <div class="form-row">
-                        <div class="form-group col-md-3">
-                            <label for="selectMaterialEdit" style="color: #151631; font-family: 'Segoe UI'; font-weight: 700;">Material</label>
-                            <select id="selectMaterialEdit" style="width: 110%;" class="selectMaterial" class="form-control" name="selectMaterialEdit">
-                                <option></option>
-                                @foreach($materiais as $material)
-                                    <option value="{{$material->id}}"> {{$material->codigo}} - {{ $material->nome }} </option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group col-md-2" style="margin-left: 4%">
-                            <label for="InputQuantEdit" style="color: #151631; font-family: 'Segoe UI'; font-weight: 700">Quantidade</label>
-                            <input type="number" min="1" onkeypress="return onlyNums(event,this);" class="form-control" id="InputQuantEdit" name="InputQuantEdit" value="{{ old('quantidade') }}">
-                        </div>
-                        <div class="form-group">
-                            <button id="updateMaterial" style="margin-top: 30px; margin-left: 10px" class="btn btn-primary" onclick="confirmarAlteracao()">Atualizar</button>
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalLabel" style="color:#151631">Editar Solicitação</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div id="modalBody">
+                        <div class="form-row">
+                            <div class="form-group col-md-3">
+                                <label for="selectMaterialEdit" style="color: #151631; font-family: 'Segoe UI'; font-weight: 700;">Material</label>
+                                <select id="selectMaterialEdit" style="width: 110%;" class="selectMaterial" class="form-control" name="selectMaterialEdit">
+                                    <option></option>
+                                    @foreach($materiais as $material)
+                                        <option value="{{$material->id}}"> {{$material->codigo}} - {{ $material->nome }} </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group col-md-2" style="margin-left: 4%">
+                                <label for="InputQuantEdit" style="color: #151631; font-family: 'Segoe UI'; font-weight: 700">Quantidade</label>
+                                <input type="number" min="1" onkeypress="return onlyNums(event,this);" class="form-control" id="InputQuantEdit" name="InputQuantEdit" value="{{ old('quantidade') }}">
+                            </div>
+                            <div class="form-group">
+                                <button id="updateMaterial" style="margin-top: 30px; margin-left: 10px" class="btn btn-primary" onclick="confirmarAlteracao()">Atualizar</button>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-          </div>
         </div>
     </div>
 @endsection
