f12a4d2f7fbb33f481660f1c5591588f090702ae
Douglas Batista <batista.douglasfs@gmail.com>
2022-06-06 09:21:40 -0300

    Remoção da logo da UPE no e-mail e confirmação

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            @if (trim($slot) === 'Laravel')
-                <img src="https://upload.wikimedia.org/wikipedia/commons/9/9b/Logo-upe-site.png" class="logo" alt="Laravel Logo" width="200" height="150">
-            @else
-                {{ $slot }}
-            @endif
+            {{ $slot }}
         </a>
     </td>
 </tr>
