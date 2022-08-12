c4328adff8f821f2a51e7b57c0837b9d040ae5c6
Douglas Batista <batista.douglasfs@gmail.com>
2022-08-12 17:26:39 -0300

    Ocultação das datas de início e fim da página de geração de relatórios

--- public/js/relatorio/relatorio_escolha.js
+++ public/js/relatorio/relatorio_escolha.js
@@ -5,3 +5,17 @@ $(function() {
     document.getElementById('data_fim').max = new Date(new Date().getTime() - new Date()
     .getTimezoneOffset() * 60000).toISOString().split("T")[0];
 });
+
+$(function() {
+    $("#tipo_relatorio").change(function()
+    {
+        if($("#tipo_relatorio").val() == 4 || $("#tipo_relatorio").val() == 6) {
+            $("#data_inicio").hide();
+            $("#data_fim").hide();
+        } else {
+            $("#data_inicio").show();
+            $("#data_fim").show();
+        }
+    });
+});
+
