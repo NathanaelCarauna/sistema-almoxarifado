7e22864a4f994d2eaa46d43e67b0a3c310428677
Douglas Batista <batista.douglasfs@gmail.com>
2022-08-26 17:12:34 -0300

    Correção: desabilitar o botão Reistrar Estoques após o primeiro clique

--- public/js/movimento/entrada_material.js
+++ public/js/movimento/entrada_material.js
@@ -3,3 +3,10 @@ $(function () {
         placeholder: "Digite algo ou selecione uma opção."
     });
 });
+
+$(function () {
+    $("#entrada").on("click", function(){
+        $("#form_entrada").submit();
+        $(this).prop("disabled", true);
+    });
+});
